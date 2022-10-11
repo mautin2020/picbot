5eb8e31a0b9ca89ab0aceee411b27909ecf644a2
mautin2020 <75550350+mautin2020@users.noreply.github.com>
2022-10-11 20:12:49 +0100

    Create imageDisplay.vue

---
+++ imageDisplay.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+    <!-- we are assuming we are having a css class call modalCard that will display the card -->
+    <div v-for="(image, index) in images" :key="index" 
+        class="modalCard">
+        <img :src="image" width="50" height="50" 
+            @click="displayImage(index)">
+    </div>
+</div>
+</template>
+
+<script>
+    
+    export default {
+        data() {
+            return {
+                // the modal display is initially set to false which will be bind to div element
+                modalDispaly: false,
+                
+                // images array
+                images: [
+                    {
+                        image1,
+                        image2,
+                        image3,
+                        image4
+                    }
+                ]
+            }
+        },
+
+        methods: {
+            displayImage(index) 
+            {
+                this.modalDispaly(index) = true;
+            }
+        }
+    }
+    
+</script>
+
+<style>
+
+</style>
